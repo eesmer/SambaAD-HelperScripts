b7ead2c7387f56e1737c207bb10beb36ad64ca0c
erkan.esmer <erkanesmer@gmail.com>
2024-08-15 15:20:13 +0300

    additional-dc-joiner script edited

--- scripts/samba-activedirectory-additional-dc-joiner.sh
+++ scripts/samba-activedirectory-additional-dc-joiner.sh
@@ -13,3 +13,28 @@ whiptail --msgbox \
     \n\nhttps://github.com/eesmer/SambaAD-HelperScripts \
     \nhttps://github.com/eesmer/sambadtui \
     \nhttps://github.com/eesmer/DebianDC" 25 90 45
+
+HNAME=$(whiptail --inputbox "Enter the DC Machine Hostname without FQDN (e.g.,DC02)" 10 50 --title "DC Hostname" --backtitle "DC Hostname" 3>&1 1>&2 2>&3)
+ANSWER=$?
+        if [ ! $ANSWER = 0 ]; then
+                echo "User canceled"
+                exit 1
+        fi
+DNSSRV=$(whiptail --inputbox "Enter the DNS Server IP Address" 10 50 --title "DNS Server Info" --backtitle "DNS Server Info" 3>&1 1>&2 2>&3)
+ANSWER=$?
+        if [ ! $ANSWER = 0 ]; then
+                echo "User canceled"
+                exit 1
+        fi
+DOMAIN=$(whiptail --inputbox "Enter the Domain Name (e.g., example.lan)" 10 50 --title "Domain Name" --backtitle "Domain Name" 3>&1 1>&2 2>&3)
+ANSWER=$?
+        if [ ! $ANSWER = 0 ]; then
+                echo "User canceled"
+                exit 1
+        fi
+PASSWORD=$(whiptail --passwordbox "Enter the Administrator Password" 10 50 --title "Administrator Password" --backtitle "Administrator Password" 3>&1 1>&2 2>&3)
+ANSWER=$?
+        if [ ! $ANSWER = 0 ]; then
+                echo "User canceled"
+                exit 1
+        fi
