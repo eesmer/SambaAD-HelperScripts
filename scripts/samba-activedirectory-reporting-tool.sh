5fb58e87a2b3c3ba181ba8e869fe6fde54766d7e
erkan.esmer <erkanesmer@gmail.com>
2024-08-18 10:52:06 +0300

    reporting script edited

--- scripts/samba-activedirectory-reporting-tool.sh
+++ scripts/samba-activedirectory-reporting-tool.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Color Codes
+MAGENTA="tput setaf 1"
+GREEN="tput setaf 2"
+YELLOW="tput setaf 3"
+DGREEN="tput setaf 4"
+CYAN="tput setaf 6"
+WHITE="tput setaf 7"
+GRAY="tput setaf 8"
+RED="tput setaf 9"
+BLUE="tput setaf 12"
+NOCOL="tput sgr0"
+BOLD="tput bold"
+
 CHECKRUN_ROOT() {
 if ! [[ $EUID -eq 0 ]]; then
         $RED
